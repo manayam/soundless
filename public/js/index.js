1de5ec8597236d2beb7be5b9d90c021bfb6076cd
Vivian Ta <vivian.vta@gmail.com>
2018-02-07 20:52:37 -0800

    public files

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidemenu").style.width = "170px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidemenu").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
